213f22069138f608c5a4a80cae70e19e09034712
Shahryar Masoumi <shahryar@LBNL731458.corp.capgemini.com>
2023-02-16 11:35:56 +0100

    new react files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,30 +2,94 @@ import "./App.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import TodoView from "./components/TodoListView";
 function App() {
-    return (
-        <div className="App">
-            <div
-                className="App list-group-item justify-content-center mx-auto"
-                style={{
-                    width: "400px",
-                    backgroundColor: "white",
-                    marginTop: "15px",
-                }}
-            >
-                <h1
-                    className="card text-white bg-primary mb-1"
-                    styleName="max-width: 20rem;"
-                >
-                    Task Manager
-                </h1>
-                <h6 className="card text-white bg-primary mb-3">
-                    FASTAPI - React - MongoDB
-                </h6>
-            </div>
+  const [todoList, setTodoList] = useState([{}]);
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+
+  // Read all todos
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/todo").then((res) => {
+      setTodoList(res.data);
+    });
+  });
+
+  // Post a todo
+  const addTodoHandler = () => {
+    axios
+      .post("http://localhost:8000/api/todo/", {
+        title: title,
+        description: desc,
+      })
+      .then((res) => console.log(res));
+  };
+
+  return (
+    <div
+      className="App list-group-item justify-content-center align-items-center mx-auto"
+      style={{
+        width: "400px",
+        backgroundColor: "white",
+        marginTop: "15px",
+        border: "1px solid grey",
+        padding: "20px",
+      }}
+    >
+      <h1
+        className="card text-white bg-primary mb-1"
+        styleName="max-width: 20rem;"
+      >
+        Task Manager
+      </h1>
+      <h6 className="card text-white bg-primary mb-3">
+        FASTAPI - React - MongoDB
+      </h6>
+      <div className="card-body">
+        <h5 className="card text-white bg-dark mb-3">Add Your Task</h5>
+        <span className="card-text">
+          <input
+            className="mb2 form-control titleIn"
+            placeholder="Title"
+            style={{ "margin-top": "10px" }}
+            onChange={(event) => setTitle(event.target.value)}
+          ></input>
+          <input
+            className="mb2 form-control desIn"
+            placeholder="Description"
+            style={{ "margin-top": "10px" }}
+            onChange={(event) => setDesc(event.target.value)}
+          ></input>
+          <button
+            className="btn btn-outline-primary mx-2"
+            style={{
+              borderRadius: "50px",
+              "font-weight": "bold",
+              "margin-top": "10px",
+            }}
+            onClick={addTodoHandler}
+          >
+            Add task
+          </button>
+        </span>
+        {/* adding the task */}
+        <h5
+          className="card text-white bg-dark mb-3"
+          style={{
+            "margin-top": "10px",
+          }}
+        >
+          Your Tasks
+        </h5>
+        <div>
+          {/* exteral component */}
+          <h6 className="card text-dark bg-warning py-1 mb-0">
+            2023, All rights reserved &copy;
+          </h6>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/components/TodoListView.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TodoItem from "./Todo";
+
+function TodoView(props) {
+  return (
+    <div>
+      <ul>
+        {props.todoList.map((todo) => (
+          <TodoItem todo={todo} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default TodoView;
